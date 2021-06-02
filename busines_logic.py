3fdb1c58f797d0c7ce5eded39f689b725ca4675a
grunsen <nikitagrunsky@gmail.com>
2021-06-02 14:47:52 +0000

    RKSOK works

--- busines_logic.py
+++ busines_logic.py
@@ -3,11 +3,12 @@ import asyncio
 
 
 async def serve_client(reader, writer):
+    '''The function reads data, processes requests, and sends a response to the client'''
     request = await read_request(reader)
     response = await handle_request(request)
-    await write_response(writer, response)
+    await send_response(writer, response)
 
-async def read_request(reader):
+async def read_request(reader) -> str:
     request = b""
     while True:
         data = await reader.read(1024)
@@ -15,58 +16,73 @@ async def read_request(reader):
         request += data
         return request.decode('UTF-8')
 
-async def handle_request(request):
-    name = determine_name(request)
-    if len(name) > 30 or not request.endswith("\r\n\r\n"):
+async def handle_request(request: str) -> bytes:
+    if request == None:
         response = "НИПОНЯЛ РКСОК/1.0"
         return response.encode('UTF-8')
-    response_from_authorities = await ask_permission(request)
-    if response_from_authorities != "possible":
-        return response_from_authorities.encode('UTF-8')
+    name = determine_name(request)
+    standart = determine_standart(request)
+    if len(name) > 30 or not request.endswith("\r\n\r\n") or standart != "РКСОК/1.0":
+        response = "НИПОНЯЛ РКСОК/1.0"
+        return response.encode('UTF-8')
     method = determine_method(request)
     if method == "ЗОПИШИ":
-        await write_phone(request)
+        response_from_authorities = await ask_permission(request)
+        if response_from_authorities != "possible":
+            return response_from_authorities.encode('UTF-8')
+        await write_down_phone(request)
         response = 'НОРМАЛДЫКС РКСОК/1.0'
         return response.encode('UTF-8')
     elif method == "ОТДОВАЙ":
+        response_from_authorities = await ask_permission(request)
+        if response_from_authorities != "possible":
+            return response_from_authorities.encode('UTF-8')
         response = await get_phone(request)
         return response.encode('UTF-8')
     elif method == "УДОЛИ":
+        response_from_authorities = await ask_permission(request)
+        if response_from_authorities != "possible":
+            return response_from_authorities.encode('UTF-8')
         response = await delete_phone(request)
         return response.encode('UTF-8')
     else:
         response = "НИПОНЯЛ РКСОК/1.0"
         return response.encode('UTF-8')
 
-async def write_response(writer, response):
+async def send_response(writer, response):
     writer.write(response)
     await writer.drain()
     writer.close()
 
+def determine_standart(request: str) -> str:
+    standart = request.split('\r\n')[0].split(' ')[-1]
+    return standart
+
 def determine_method(data: str) -> str:
-    return data.split('\r\n')[0].split(' ')[0]
+    method = data.split('\r\n')[0].split(' ')[0]
+    return method
 
 def determine_name(data: str) -> str:
-    return " ".join(data.split('\r\n')[0].split()[1:-1])
+    name = " ".join(data.split('\r\n')[0].split()[1:-1])
+    return name
 
 def determine_phone(data: str) -> list:
     phones = []
     strings = data.split('\r\n')[1:]
-    print(strings)
     for string in strings:
         if string != "":
             phones.append(string)
     return phones
 
-async def write_phone(data: str) -> None:
-    name = determine_name(data) # Str
-    phones = determine_phone(data) # List
+async def write_down_phone(data: str) -> None:
+    name = determine_name(data)
+    phones = determine_phone(data)
     new_entry = f"{name}"
     for phone in phones:
         new_entry += f"!!!{phone}"
     new_entry += "\n"
     async with aiofiles.open("phonebook.txt", mode='r') as f:
-        phonebook = await f.readlines()   
+        phonebook = await f.readlines()
     for index, value in enumerate(phonebook):
         if value == "":
             del phonebook[index]
@@ -86,7 +102,6 @@ async def get_phone(data: str) -> str:
     for line in phonebook:
         if line.split("!!!")[0] == name:
             phones = "\r\n".join(line.split("!!!")[1:])
-            print("Phones: ", phones)
             answer = "НОРМАЛДЫКС РКСОК/1.0\r\n" + phones
             return answer
         else:
@@ -116,6 +131,7 @@ async def delete_phone(data: str) -> str:
         return answer
 
 async def ask_permission(data: str, host: str = "vragi-vezde.to.digital", port: int = 51624) -> str:
+    '''Sending a request to the verification server'''
     reader, writer = await asyncio.open_connection(host, port)
     request_body = f"АМОЖНА? РКСОК/1.0\r\n+{data}".encode("UTF-8")
     writer.write(request_body)
--- server.py
+++ server.py
@@ -3,7 +3,7 @@ import os
 from busines_logic import serve_client
 
 
-async def run_server(host: str = "0.0.0.0", port: int = 3333):
+async def run_server(host: str = "0.0.0.0", port: int = 8000):
     if not os.path.exists("phonebook.txt"):
         with open("phonebook.txt", 'w') as f:
             f.write("")
